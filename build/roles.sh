c573e0e99d2d84046642364bd275496395c6a3bd
Nils <git@cyclenerd.de>
2025-12-15 21:06:07 +0100

    role-permission-filter new url

--- build/roles.sh
+++ build/roles.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Get role permission filter... Please wait..."
-curl -O "https://cloud.google.com/iam/json/role-permission-filter.json"
+# Source: https://docs.cloud.google.com/iam/docs/roles-permissions
+curl -O "https://docs.cloud.google.com/iam/json/role-permission-filter.json"
 
 echo "Get roles... Please wait..."
 gcloud iam roles list --quiet \
